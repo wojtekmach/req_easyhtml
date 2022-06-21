5588387e0c987797266534d4eaae46ed692ddeef
Wojtek Mach <wojtek@wojtekmach.pl>
2022-06-21 14:07:46 +0200

    Update req

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule ReqEasyHTML.MixProject do
 
   defp deps do
     [
-      {:req, github: "wojtekmach/req"},
+      {:req, "~> 0.3.0"},
       {:easyhtml, github: "wojtekmach/easyhtml"}
     ]
   end
